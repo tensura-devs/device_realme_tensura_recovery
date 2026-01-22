38013c5047f6c7564a84d86ea90b3989c32962c6
Dekuzen Okahiro <dokahiro92@gmail.com>
2026-01-22 23:39:25 +0000

    Initial commit: TWRP tree for realme 10 RMX3630 based on C.25

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for oplus ossi (ossi)
-
-```
 #
 # Copyright (C) 2026 The Android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_ossi-user
+add_lunch_combo omni_ossi-userdebug
+add_lunch_combo omni_ossi-eng
