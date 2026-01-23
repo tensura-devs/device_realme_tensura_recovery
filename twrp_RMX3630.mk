555a3da2af2c97925732080afd90f3ac4794405a
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-01-23 19:16:57 +0800

    Update copyright and product definitions in mk file

--- twrp_RMX3630.mk
+++ twrp_RMX3630.mk
@@ -1,27 +1,25 @@
 #
-# Copyright (C) 2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
+# Copyright 2025 The Android Open Source Project
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+# Define hardware platform
+PRODUCT_RELEASE_NAME := RMX3630
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# Device path for OEM device tree
+DEVICE_PATH := device/realme/$(PRODUCT_RELEASE_NAME)
 
-# Inherit from RMX3630 device
-$(call inherit-product, device/oplus/RMX3630/device.mk)
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Inherit any OrangeFox-specific settings
+$(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Product Specifics
-PRODUCT_NAME := twrp_RMX3630
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX3630
-PRODUCT_BRAND := Realme
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3630
 PRODUCT_MANUFACTURER := realme
-
-PRODUCT_GMS_CLIENTID_BASE := android-oppo
