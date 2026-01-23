cec948a87f15b79cf0f58053d54c29a2941e8ca1
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-01-23 18:15:18 +0800

    Define RMX3630 device configuration in mk file
    Add device configuration for RMX3630 including paths and product details.

--- twrp_RMX3630.mk
+++ twrp_RMX3630.mk
@@ -2,12 +2,21 @@
 # Copyright 2025 The Android Open Source Project
 #
 
+# Define hardware platform
+PRODUCT_RELEASE_NAME := RMX3630
+
+# Device path for OEM device tree
+DEVICE_PATH := device/oplus/ossi/$(PRODUCT_RELEASE_NAME)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit from RMX3630 device
-$(call inherit-product, device/oplus/ossi/RMX3630/device.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -18,3 +27,5 @@ PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3630
 PRODUCT_MANUFACTURER := realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
