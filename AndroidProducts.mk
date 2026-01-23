f9f0e34970e67978bf1e7586033c8eea9244b5c4
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-01-23 19:15:02 +0800

    Modify copyright information in AndroidProducts.mk
    Updated copyright year and project name.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,7 @@
 #
-# Copyright (C) 2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025 The TWRP Open Source Project
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_RMX3630.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_RMX3630.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_RMX3630-user \
-    twrp_RMX3630-userdebug \
-    twrp_RMX3630-eng
+COMMON_LUNCH_CHOICES := twrp_RMX3630-eng
