80f9c51a583576469b276b20cb148a648d2229d3
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-01-23 18:27:06 +0800

    Update copyright and target device in Android.mk

--- Android.mk
+++ Android.mk
@@ -1,12 +1,13 @@
 #
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),ossi)
+ifeq ($(TARGET_DEVICE), RMX3630)
+
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
 endif
