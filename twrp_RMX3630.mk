665e7647222841db38d7a89f231539283e89e014
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-01-23 18:10:43 +0800

    Update twrp_RMX3630.mk for product inheritance
    Refactor product inheritance and device path definitions.

--- twrp_RMX3630.mk
+++ twrp_RMX3630.mk
@@ -2,17 +2,12 @@
 # Copyright 2025 The Android Open Source Project
 #
 
-# Define hardware platform
-PRODUCT_RELEASE_NAME := RMX3630
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Device path for OEM device tree
-DEVICE_PATH := device/oplus/ossi/$(PRODUCT_RELEASE_NAME)
-
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
-
-# Inherit any OrangeFox-specific settings
-$(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
+# Inherit from RMX3630 device
+$(call inherit-product, device/oplus/ossi/RMX3630/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
