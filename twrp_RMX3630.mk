e284d05e03534819991297c90ef4e924309ff501
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-01-23 18:29:23 +0800

    Modify copyright and product details in mk file
    Updated copyright information and product specifics.

--- twrp_RMX3630.mk
+++ twrp_RMX3630.mk
@@ -1,15 +1,8 @@
 #
-# Copyright 2025 The Android Open Source Project
+# Copyright (C) 2022 The LineageOS Project
 #
-
-# Define hardware platform
-PRODUCT_RELEASE_NAME := RMX3630
-
-# Device path for OEM device tree
-DEVICE_PATH := device/oplus/ossi/$(PRODUCT_RELEASE_NAME)
-
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
+# SPDX-License-Identifier: Apache-2.0
+#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -18,13 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Inherit from RMX3630 device
+$(call inherit-product, device/oplus/RMX3630/device.mk)
+
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-## Device identifier. This must come after all inclusions
+# Product Specifics
+PRODUCT_NAME := twrp_RMX3630
 PRODUCT_DEVICE := RMX3630
-PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
-PRODUCT_BRAND := realme
+PRODUCT_BRAND := Realme
 PRODUCT_MODEL := RMX3630
 PRODUCT_MANUFACTURER := realme
 
