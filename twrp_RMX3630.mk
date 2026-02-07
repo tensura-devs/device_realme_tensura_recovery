f11441167b2d05a9cdffb211a430b348883d4e81
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-02-07 20:02:32 +0800

    Update twrp_RMX3630.mk with device configuration

--- twrp_RMX3630.mk
+++ twrp_RMX3630.mk
@@ -4,15 +4,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from RMX3630 device
-$(call inherit-product, device/realme/RMX3630/device.mk)
+#
+# Copyright 2025 The Android Open Source Project
+#
+
+# Define hardware platform
+PRODUCT_RELEASE_NAME := RMX3630
+
+# Device path for OEM device tree
+DEVICE_PATH := device/realme/$(PRODUCT_RELEASE_NAME)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Product Specifics
-PRODUCT_NAME := twrp_RMX3630
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX3630
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3630
 PRODUCT_MANUFACTURER := realme
