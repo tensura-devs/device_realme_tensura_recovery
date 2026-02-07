d3d1fc47f6032ea0b131fe0de177c723f6d01c8a
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-02-07 15:55:35 +0800

    Fix brand name casing in twrp_RMX3630.mk
    Updated the brand name from 'Realme' to 'realme' for consistency.

--- twrp_RMX3630.mk
+++ twrp_RMX3630.mk
@@ -4,13 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Inherit from RMX3630 device
 $(call inherit-product, device/oplus/RMX3630/device.mk)
 
@@ -20,7 +13,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Product Specifics
 PRODUCT_NAME := twrp_RMX3630
 PRODUCT_DEVICE := RMX3630
-PRODUCT_BRAND := Realme
+PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3630
 PRODUCT_MANUFACTURER := realme
 
