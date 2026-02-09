533e1e0cc7b67f800fdd64047ee5efb8bc80b266
Dekuzen Okahiro <dokahiro92@gmail.com>
2026-02-09 20:33:29 +0000

    Fix several stuff and dynamic partitions

--- device.mk
+++ device.mk
@@ -105,9 +105,15 @@ PRODUCT_PACKAGES += \
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
-    android.hardware.keymaster@4.1
+    android.hardware.gatekeeper@1.0 \
+    android.hardware.keymaster@4.1 \
+    libkeymaster4_1support \
+    init.recovery.mt6789.rc \
+    init.recovery.logd.rc
 
+# Copy init scripts to recovery & Services libraries
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1 \
+    $(TARGET_OUT_VENDOR)/bin/hw/android.hardware.gatekeeper@1.0-service
