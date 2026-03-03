b6a28af2478b41d3a1b23e62fd6e1befdf6163d8
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-03-03 23:38:36 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -115,7 +115,7 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/vendor/firmware/tp/140956/FW_FT3518_SAMSUNG_FAE.img:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/tp/140956/FW_FT3518_SAMSUNG.img \
-    $(LOCAL_PATH)/recovery/root/sbin/tp_fw_flash.sh:$(TARGET_COPY_OUT_RECOVERY)/root/sbin/tp_fw_flash.sh
+    $(LOCAL_PATH)/recovery/root/system/bin/tp_fw_flash.sh:$(TARGET_COPY_OUT_RECOVERY)/root/sbin/tp_fw_flash.sh
 
 # Copy init scripts to recovery & Services libraries
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
