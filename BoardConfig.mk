f60f6634d8c2afea0e591a423c929cdb0e34d197
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-03-04 10:55:38 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,6 +196,7 @@ TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
 TW_DELAY_TOUCH_INIT_MS := 500
 TW_NO_HAPTICS := true
 TW_INPUT_BLACKLIST := "nt36672c ilitek7807s synaptics-s3910"
+TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone1/temp
 
 # Ensure touch input works properly in recovery
 RECOVERY_TOUCHSCREEN_SWAP_XY := false
