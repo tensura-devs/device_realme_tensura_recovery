af4ce45093354567f3b44107bb8b2d945023be40
DekuzenOkahiro <72677591+DekuzenOkahiro@users.noreply.github.com>
2026-06-11 16:57:59 +0800

    Add device configuration for RMX3630

---
+++ twrp_RMX3630.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#
+# Copyright 2025 The Android Open Source Project
+#
+
+# Define hardware platform
+PRODUCT_RELEASE_NAME := RMX3630
+
+# Device path for OEM device tree
+DEVICE_PATH := device/realme/$(PRODUCT_RELEASE_NAME)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := RMX3630
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3630
+PRODUCT_MANUFACTURER := realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
